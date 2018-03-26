ffbca7fd1328f7601f01ba2d370ead8dba09c1c2
Lee <326656669@qq.com>
2018-03-26 20:19:10 +0800

    添加登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -9,11 +9,41 @@ let app = new Vue({
       birthday: '1993.06',
       wantedJob: '前端',
       content: '13000000000'
+    },
+    login:{
+      email:'',
+      password:''
+    },
+    signUp:{
+      email:'',
+      password:''
     }
   },
   methods: {
     onEdit (key, value) {
       this.resume[key] = value
+    },
+    onLoginIn(){
+      AV.User.logIn(this.login.password, this.login.email).then(function (user) {
+        console.log(user);
+      }, function (error) {
+        if(error.code === 211){
+          alert('邮箱不存在')
+        }else if(error.code === 210){
+          alert('邮箱和密码不匹配')
+        }
+      })
+    },
+    onSignUp(){
+      console.log(this.signUp)
+      const user = new AV.User()
+      user.setUsername(this.signUp.email)
+      user.setPassword(this.signUp.password)
+      user.setEmail(this.signUp.email)
+      user.signUp().then(function (user) {
+        console.log(user);
+      }, function (error) {
+      });
     },
     onClickSave () {
       let currentUser = AV.User.current()
@@ -22,14 +52,17 @@ let app = new Vue({
       } else {
         this.saveResume()
       }
-      // console.log(this.resume)
-      // let User = AV.Object.extend('User')
-      // let user = new User()
-      // user.set('resume',this.resume)
-      // user.save().then(function () {
-      // }, function (error) {
-      // })
     },
-    saveResume () {}
+    saveResume () {
+      let {id} = AV.User.current()
+      let user = AV.Object.createWithoutData('User', id)
+      user.set('resume',this.resume)
+      user.save()
+    },
+    onLogout(){
+      AV.User.logOut()
+      alert('注销成功')
+      window.location.reload()
+    }
   }
 })
