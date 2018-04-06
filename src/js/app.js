b7fc9ef373448ce17fa476bf834c269b6cdf6b56
Lee <326656669@qq.com>
2018-04-06 18:28:54 +0800

    添加项目模块

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,10 @@ let app = new Vue({
         {name: '请填写技能名称', description: '请填写技能描述'},
         {name: '请填写技能名称', description: '请填写技能描述'},
         {name: '请填写技能名称', description: '请填写技能描述'}
+      ],
+      projects: [
+        {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请填写项目描述'},
+        {name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请填写项目描述'}
       ]
     },
     login: {
@@ -121,6 +125,12 @@ let app = new Vue({
     },
     removeSkill (index) {
       this.resume.skills.splice(index, 1)
+    },
+    addProject(){
+      this.resume.projects.push({name: '请填写项目名称', link: 'http://...', keywords: '请填写关键词', description: '请填写项目描述'})
+    },
+    removeProject(index){
+      this.resume.projects.splice(index,1)
     }
   }
 })
